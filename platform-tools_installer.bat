7b77122fc5593c59ab4792059c88cf7313c12a28
Sunset <grigory.orlov.set@gmail.com>
2022-10-12 00:46:08 +0500

    correct mistakes

--- platform-tools_installer.bat
+++ platform-tools_installer.bat
@@ -1,16 +1,16 @@
 @echo off
 title Platform Tools Installer
 setlocal EnableDelayedExpansion
-if not "%~1"=="" set archive_path=%~1 && goto :unpack 
+if not "%~1"=="" set "archive_path=%~1" && goto :unpack
 :download
-echo. && echo.Скачивание Platform Tools...
+echo. && echo.Скачивание Platform tools...
 if exist !userprofile!\platform-tools rmdir /s /q !userprofile!\platform-tools
 powershell -Command "& {Invoke-WebRequest https://dl.google.com/android/repository/platform-tools-latest-windows.zip -outfile !userprofile!\platform-tools.zip}"
-archive_path=!userprofile!\platform-tools.zip
+set "archive_path=%userprofile%\platform-tools.zip"
 CLS
 :unpack
-echo. && echo.Распаковка Platform Tools...
-powershell -Command "& {Expand-Archive -Path !archive_path!  -DestinationPath !userprofile! -Force}"
+echo. && echo.Распаковка Platform tools...
+powershell -Command "& {Expand-Archive -Path !archive_path! -DestinationPath !userprofile! -Force}"
 CLS
 :add_to_path
 echo. && echo. Добавление в PATH...
@@ -19,4 +19,3 @@ if %errorlevel% NEQ 0 (powershell -Command "& {[Environment]::SetEnvironmentVari
 del /f /q !userprofile!\platform-tools.zip
 CLS && echo. && echo.Успешно
 echo. && pause && exit
-exit
